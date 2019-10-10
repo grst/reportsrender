6d764d92b033344a353dec015b3890b5160a657a
Gregor Sturm <mail@gregor-sturm.de>
2019-10-10 16:07:20 +0200

    add entry points

---
+++ reportsrender/cli.py
@@ -0,0 +1,48 @@
+"""reportsrender
+
+Execute and render a jupyter/Rmarkdown notebook.
+
+Usage:
+  reportsrender <engine> <notebook> <out_file> [options]
+  reportsrender --help
+
+Options:
+  -h --help             Show this screen.
+  --cpus=<cpus>         Number of CPUs to use for Numba/Numpy/OpenBLAS/MKL [default: 1]
+  --params=<params>     space-separated list of key-value pairs that will be passed
+                        to papermill/Rmarkdown. 
+                        E.g. "input_file=dir/foo.txt output_file=dir2/bar.html"
+
+Possible engines are:
+    rmd             Use `rmarkdown` to execute the notebook. Supports R and
+                    python (through reticulate)
+    papermill       User `papermill` to execute the notebook. Works for every
+                    kernel available in the jupyter installation.
+
+"""
+
+from docopt import docopt
+from .util import set_cpus, parse_params
+from .papermill import render_papermill
+from .rmd import render_rmd
+import sys
+
+
+def main():
+    arguments = docopt(__doc__)
+    params = parse_params(arguments["--params"])
+    set_cpus(arguments["--cpus"])
+
+    if arguments["<engine>"] == "rmd":
+        render_rmd(arguments["<notebook>"], arguments["<out_file>"], params)
+    elif arguments["<engine>"] == "papermill":
+        render_papermill(arguments["<notebook>"], arguments["<out_file>"], params)
+    else:
+        print(
+            "Please specify a valid engine. See `reportsrender --help` for more details. ",
+            file=sys.stderr,
+        )
+
+
+if __name__ == "__main__":
+    main()
