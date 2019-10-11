6f826af944c2806d4a43687cffd61d8c44f08a33
Gregor Sturm <mail@gregor-sturm.de>
2019-10-11 13:08:55 +0200

    enable build matrix

--- reportsrender/__init__.py
+++ reportsrender/__init__.py
@@ -1,2 +1,8 @@
+"""Execute and Render notebooks as HTML reports. """
+from get_version import get_version
+
+__version__ = get_version(__file__)
+del get_version
+
 from .papermill import render_papermill
 from .rmd import render_rmd
